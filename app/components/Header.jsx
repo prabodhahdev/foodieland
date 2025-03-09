882e6949e4fa9ff25e7770780ce1b79c417832d7
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2025-03-09 23:46:10 +0530

    Initial Commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import {   FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <div className=' flex items-center justify-between py-5  ] border-b border-gray-400'>
+        <div>
+         <Image src="/logo.png" width={150} height={50} alt='Logo'/>
+        </div>
+      
+      <ul className='flex items-center '>
+        <li className='hover:text-[#FF7426]'><Link href="/">Home</Link></li>
+        <li className='hover:text-[#FF7426]'><Link href="/recipes">Recipes</Link></li>
+        <li className='hover:text-[#FF7426]'><Link href="/blog">Blog</Link></li>
+        <li className='hover:text-[#FF7426]'><Link href="/contact">Contact</Link></li>
+        <li className='hover:text-[#FF7426]'><Link href="/about">About Us</Link></li>
+      </ul>
+     <div className='flex items-center'>
+     <FaFacebookF size={55} />
+     <FaTwitter size={60} />
+     <FaInstagram size={60} />
+     </div>
+    
+    
+    </div>
+  )
+}
+
+export default Header
---
+++ app/recipes/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     recipes 
+    </div>
+  )
+}
+
+export default page
